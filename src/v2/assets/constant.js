fbc68e1a30923f2d2fcf6bf377a2da6d04f20e82
Pranjal <pranjalgain052@gmail.com>
2021-07-21 21:37:22 +0530

    Link added to footer

--- src/v2/assets/constant.js
+++ src/v2/assets/constant.js
@@ -2,6 +2,6 @@ export const EMAIL_ADDRESS = 'pranjalgain052@gmail.com';
 export const MOBILE_NO = 9800384120;
 export const FACEBOOK_ADDRESS = "https://www.facebook.com/pranjal.gain.5/";
 export const INSTAGRAM_ADDRESS = "https://www.instagram.com/pranjalgain/";
-export const LINKEDIN_ADDRESS = 'pranjalgain052@gmail.com';
-export const GITHUB_ADDRESS = "https://www.linkedin.com/in/pranjal-gain-648b5b109/";
+export const LINKEDIN_ADDRESS = "https://www.linkedin.com/in/pranjal-gain-648b5b109/";
+export const GITHUB_ADDRESS = "https://github.com/pranjalb21";
 
--- src/v2/components/home/footer.jsx
+++ src/v2/components/home/footer.jsx
@@ -5,6 +5,7 @@ import InstagramIcon from "@material-ui/icons/Instagram";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import { useStyle } from "./style";
+import { FACEBOOK_ADDRESS, INSTAGRAM_ADDRESS, LINKEDIN_ADDRESS, GITHUB_ADDRESS } from '../../assets/constant';
 
 
 const Footer = () => {
@@ -19,16 +20,16 @@ const Footer = () => {
         direction="row"
         className={classes.footer}
       >
-        <IconButton>
+        <IconButton href={FACEBOOK_ADDRESS} target="_blank">
           <FacebookIcon style={{ color: "#129AF6" }} fontSize="large" />
         </IconButton>
-        <IconButton>
+        <IconButton href={INSTAGRAM_ADDRESS} target="_blank">
           <InstagramIcon style={{ color: "#E1306C" }} fontSize="large" />
         </IconButton>
-        <IconButton>
+        <IconButton href={GITHUB_ADDRESS} target="_blank">
           <GitHubIcon style={{ color: "white" }} fontSize="large" />
         </IconButton>
-        <IconButton>
+        <IconButton href={LINKEDIN_ADDRESS} target="_blank">
           <LinkedInIcon style={{ color: "skyblue" }} fontSize="large" />
         </IconButton>
       </Grid>
