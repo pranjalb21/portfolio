cd9ad47d22cea8f1549b21aae42191d18f2f22d0
Pranjal <pranjalgain052@gmail.com>
2021-08-02 20:21:39 +0530

    Portfolio added as a new project

--- src/v2/assets/projects.js
+++ src/v2/assets/projects.js
@@ -1,5 +1,6 @@
 import stopwatch from './stopwatch.png';
 import travel from './travelling.png';
+import portfolio from './portfolio.PNG'
 const projects = [
   {
     id: 1,
@@ -17,6 +18,15 @@ const projects = [
     project_about: "A stopwatch app using the basics of react.",
     project_link: "https://stopwatch-pr.netlify.app/",
     project_code: "https://github.com/pranjalb21/stopwatch",
+  },
+  {
+    id: 3,
+    project_title: "Portfolio website",
+    project_image: portfolio,
+    project_about:
+      "This is my portfolio website. I've used ReactJs to create this project",
+    project_link: "https://pranjal-gain.netlify.app/",
+    project_code: "https://github.com/pranjalb21/portfolio",
   },
 ];
 export default projects;
