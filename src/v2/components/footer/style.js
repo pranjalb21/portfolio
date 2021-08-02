22b9488fba09259c4c4b51535f98a31a32348290
Pranjal <pranjalgain052@gmail.com>
2021-08-02 12:59:04 +0530

    Footer color changed

--- src/v2/components/footer/style.js
+++ src/v2/components/footer/style.js
@@ -1,16 +1,16 @@
 import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles({
-    footer: {
-        height: '100px',
-        marginBottom: '0',
-        backgroundColor: 'black',
-        marginRight: '0',
-        width: '100vw'
-    },
-    text: {
-        color: 'white',
-    }
-})
+  footer: {
+    height: "100px",
+    marginBottom: "0",
+    backgroundColor: "#2d3436",
+    marginRight: "0",
+    width: "100vw",
+  },
+  text: {
+    color: "white",
+  },
+});
 
 export default useStyles;
